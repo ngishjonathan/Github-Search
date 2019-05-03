0e277570193f4d3563c46c4fb4657ff446b84039
ngishjonathan <balozimwash@gmil.com>
2019-05-03 11:29:08 +0300

    created a service that allows retrive the goals github-search and inject it into the github-search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubSearchComponent } from './github-search/github-search.component';
+import { GithubsearchDetailsComponent } from './githubsearch-details/githubsearch-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubSearchComponent
+    GithubSearchComponent,
+    GithubsearchDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/githubsearch-details/githubsearch-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-githubsearch-details',
+  templateUrl: './githubsearch-details.component.html',
+  styleUrls: ['./githubsearch-details.component.css']
+})
+export class GithubsearchDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
