5c1012beba701e06681fcb35be74e0eb89ba7284
ngishjonathan <balozimwash@gmil.com>
2019-05-06 10:40:41 +0300

    added an Injector  responsible for creating the different service instances and Injecting them into classes

--- src/app/github-search.service.ts
+++ src/app/github-search.service.ts
@@ -1,14 +1,69 @@
 import { Injectable } from '@angular/core';
-import {GithubSearch} from './github-search'
+import { Http, Headers } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+
 
 @Injectable()
-export class GithubSearchService {
-  getGithubsearch(){
-    return GithubSearch;
-  }
-  constructor() {
+export class GithubService {
+    private userName: string;
+    // private clientId: string = '<Client Id>';
+    // private clientSecret: string = '<Client Secret Key>';
+    private clientId: string = '60b9f23dedffbdfc476c';
+    private clientSecret: string = '74f1322aa40990c842dcf750e380cf9a5023fbf1'                                                                                                                           
 
-  } 
+    constructor(private _http: Http) {
+        // console.log('Github Service Ready.');
+        this.userName = '';
+    }
+
+    getUser() {
+        if (this.userName) {
+            return this._http.get('http://api.github.com/users/' + this.userName
+                + '?client_id=' + this.clientId
+                + '&client_secret=' + this.clientSecret)
+                .map(res => res.json())
+                .catch(this.handleError);
+        }
+        // Bu şekilde de dönen değer üzerinden hatalar yakalanabilir. Ya da catch te....
+        // .map(res => {
+        //     console.log(res);
+        //     if (res.status != 200) {
+        //         throw new Error('This request has failed ' + res.status);
+        //     }
+        //     else {
+        //         return res.json();
+        //     }
+        // })
+    }
+
+    getRepos() {
+        if (this.userName) {
+            return this._http.get('http://api.github.com/users/' + this.userName
+                + '/repos?client_id=' + this.clientId
+                + '&client_secret=' + this.clientSecret)
+                .map(res => res.json())
+                .catch(this.handleError);
+        }
+
+    }
+
+    updateUser(userName: string) {
+        this.userName = userName;
+    }
+
+    private handleError(error: any) {
+
+        if (error.status === 401) {
+            return Observable.throw(error.status);
+        } else {
+            return Observable.throw(error.status || 'Server error');
+        }
+    }
 }
 
 
--- src/app/github-search.ts
+++ src/app/github-search.ts
@@ -1,2 +1,4 @@
 export class GithubSearch {
+  Githubsearch: any;  repositories: any;
+
 }
